9a96470b63d8aa15326748f47fce794bc20c93d9
pavel <zzzzfogzzzz@gmail.com>
2022-04-08 23:33:22 +0600

    lesson_5

---
+++ lesson_5/src/main/java/com/example/product/model/Product.java
@@ -0,0 +1,60 @@
+package com.example.product.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Long cost;
+
+    public Product(String title, Long cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Product() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getCost() {
+        return cost;
+    }
+
+    public void setCost(Long cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
